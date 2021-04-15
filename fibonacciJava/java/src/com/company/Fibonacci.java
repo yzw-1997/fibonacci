85031ec9a9eb30eca54dbce21fb68b3de1d35f84
Zhangwei Yang <yang_zhangwei@uxsino.com>
2021-04-15 15:28:51 +0800

    test improved

--- fibonacciJava/java/src/com/company/Fibonacci.java
+++ fibonacciJava/java/src/com/company/Fibonacci.java
@@ -2,25 +2,30 @@ package com.company;
 
 public class Fibonacci {
     public static int valueOfIndex(int index) {
+        int value = 0;
+
         if (index < 0){
-            throw new IllegalArgumentException("index must be a non-negative integer.");
+            throw new IllegalArgumentException("index must be non-negative.");
         }
 
-        if (index <= 1) {
-            return index;
+        if (index == 0 || index == 1){
+            value = index;
         }
 
-        int value = 1;
-        int preOne = 1;
-        int preTwo = 0;
-        for (int i = 2; i <= index; i++){
-            value = preOne + preTwo;
-            if (value < 0){
-                throw new RuntimeException("index is too  big");
-            }
-            preTwo = preOne;
-            preOne = value;
+        if (index > 1 && index <= 46){
+           int preOne = 1;
+           int preTwo = 0;
+           for (int i = 2; i <= index; i++){
+               value = preOne + preTwo;
+               preTwo = preOne;
+               preOne = value;
+           }
         }
+
+        if (index >= 47){
+            throw new IllegalArgumentException("index is too big.");
+        }
+
         return value;
     }
 }
--- fibonacciJava/java/src/com/company/test/FibonacciTest.java
+++ fibonacciJava/java/src/com/company/test/FibonacciTest.java
@@ -1,25 +1,36 @@
 package com.company.test;
 
 import com.company.Fibonacci;
-import org.junit.Ignore;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FibonacciTest {
-    Fibonacci fibonacci = new Fibonacci();
 
-    @Rule public ExpectedException thrown = ExpectedException.none();
-    @Test( expected = java.lang.IllegalArgumentException.class)
-    public void testValueOfIndex() {
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_exception_when_index_less_than_0(){
         Fibonacci.valueOfIndex(-1);
-        assertEquals(0,Fibonacci.valueOfIndex(0));
+    }
+
+    @Test
+    public void should_return_0_when_index_is_0(){
+        assertEquals(0, Fibonacci.valueOfIndex(0));
+    }
+
+    @Test
+    public void should_return_1_when_index_is_1(){
         assertEquals(1, Fibonacci.valueOfIndex(1));
-        assertEquals(13, Fibonacci.valueOfIndex(7));
-        thrown.expect(RuntimeException.class);
-        Fibonacci.valueOfIndex(2147483646);
-        Fibonacci.valueOfIndex(2147483647);
+    }
+
+    @Test
+    public void should_return_correct_value_when_index_between_2_and_46(){
+        assertEquals(1, Fibonacci.valueOfIndex(2));
+        assertEquals(55, Fibonacci.valueOfIndex(10));
+        assertEquals(1836311903, Fibonacci.valueOfIndex(46));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_exception_when_index_more_than_46(){
+        Fibonacci.valueOfIndex(47);
     }
 }
